49efe9485d2c6bbfd2b3f201b27fade049fc6d36
CHANNAKESHVA REDDY <CHANNAKESHVA REDDY@DESKTOP-9OKQOG7>
2022-03-29 17:35:31 +0530

    commit changes

--- com.crm.autodesk.SeleniumFrameWork/src/main/java/com/crm/autodesk/GenericUtility/RetryAnalyzerImplementionClass.java
+++ com.crm.autodesk.SeleniumFrameWork/src/main/java/com/crm/autodesk/GenericUtility/RetryAnalyzerImplementionClass.java
@@ -4,8 +4,15 @@ import java.util.Iterator;
 
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
-
+/**
+ * this class which is used execute failed testscript multiple times
+ * @author CHANNAKESHVA REDDY
+ *
+ */
 public class RetryAnalyzerImplementionClass implements IRetryAnalyzer{
+	/**
+	 * this method is execute multiples times until condition is satisfied
+	 */
 
 	int i=0;
 	int j=5;
